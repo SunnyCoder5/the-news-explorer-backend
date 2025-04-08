d02d952239f45a67de4a46bd70dcab6dbbfd49b8
SunnyCoder5 <masterova260989@gmail.com>
2025-04-09 01:16:29 +0300

    change config

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,7 @@
 const { JWT_SECRET = "dev-secret-key" } = process.env;
-module.exports = { JWT_SECRET };
+const isProduction = process.env.NODE_ENV === "production";
+const { dataBase = "mongodb://127.0.0.1:27017/newsExplorer_db" } = process.env;
+if (isProduction && !JWT_SECRET) {
+  throw new Error("JWT_SECRET is required in production mode");
+}
+module.exports = { JWT_SECRET, dataBase };
